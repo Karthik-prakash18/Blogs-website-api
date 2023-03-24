b17b12f43bb0c6d17c98f31c68dc4466fbbb3d43
Karthik Prakash <a2zkarthikp@gmail.com>
2023-03-24 13:47:36 -0400

    Added

--- index1.js
+++ index1.js
@@ -76,45 +76,22 @@ app.post('/logout',(req, res) =>{
     res.cookie('token','').json('ok');
 });
 
-// app.post('/post',uploadMiddleware.single('file'),async (req, res) =>{
-//     const {originalname,path}=req.file;
-//     const parts=originalname.split('.');
-//     const ext=parts[parts.length-1];
-//     const newPath=path+'.'+ext;
-//     fs.renameSync(path,newPath);
-
-//     const {token} = req.cookies;
-//     jwt.verify(token,secret,{},async (err,info)=>{
-//         if(err) throw err;
-//         const {title,summary,content} = req.body;
-//         const postDoc=await Post.create({
-//         title,
-//         summary,
-//         content,
-//         cover:newPath,
-//         author: info.id,
-//     });
-//     res.json(postDoc);
-//     });
- 
-// });
-
-app.post('/post',async (req, res) =>{
-    // const {originalname,path}=req.file;
-    // const parts=originalname.split('.');
-    // const ext=parts[parts.length-1];
-    // const newPath=path+'.'+ext;
-    // fs.renameSync(path,newPath);
+app.post('/post',uploadMiddleware.single('file'),async (req, res) =>{
+    const {originalname,path}=req.file;
+    const parts=originalname.split('.');
+    const ext=parts[parts.length-1];
+    const newPath=path+'.'+ext;
+    fs.renameSync(path,newPath);
 
     const {token} = req.cookies;
     jwt.verify(token,secret,{},async (err,info)=>{
         if(err) throw err;
-        const {title,summary,content,cover} = req.body;
+        const {title,summary,content} = req.body;
         const postDoc=await Post.create({
         title,
         summary,
         content,
-        cover,
+        cover:newPath,
         author: info.id,
     });
     res.json(postDoc);
@@ -122,55 +99,21 @@ app.post('/post',async (req, res) =>{
  
 });
 
-// app.put('/post',uploadMiddleware.single('file'),async (req, res) => {
-//     let newPath=null;
-//     if(req.file){
-//     const {originalname,path}=req.file;
-//     const parts=originalname.split('.');
-//     const ext=parts[parts.length-1];
-//     newPath=path+'.'+ext;
-//     fs.renameSync(path,newPath);
-//     }
-
-//     const {token}=req.cookies;
-//     console.log(token);
-//     jwt.verify(token,secret,{},async (err,info)=>{
-//         if(err) throw err;
-//         const {id,title,summary,content} = req.body;
-//         const postDoc=await Post.findById(id);
-//         const isAuthor= JSON.stringify(postDoc.author) === JSON.stringify(info.id);
-//         // res.json({isAuthor,postDoc,info});
-//         if(!isAuthor){
-//             return res.status(400).json('you are not the author');
-//         }
-
-//         console.log(postDoc);
-
-//         await postDoc.updateOne({
-//             title,
-//             summary,
-//             content,
-//             cover: newPath ? newPath : postDoc.cover,
-//         });
-//     res.json(postDoc);
-//     });
-// });
-
-app.put('/post',async (req, res) => {
-    // let newPath=null;
-    // if(req.file){
-    // const {originalname,path}=req.file;
-    // const parts=originalname.split('.');
-    // const ext=parts[parts.length-1];
-    // newPath=path+'.'+ext;
-    // fs.renameSync(path,newPath);
-    // }
+app.put('/post',uploadMiddleware.single('file'),async (req, res) => {
+    let newPath=null;
+    if(req.file){
+    const {originalname,path}=req.file;
+    const parts=originalname.split('.');
+    const ext=parts[parts.length-1];
+    newPath=path+'.'+ext;
+    fs.renameSync(path,newPath);
+    }
 
     const {token}=req.cookies;
     console.log(token);
     jwt.verify(token,secret,{},async (err,info)=>{
         if(err) throw err;
-        const {id,title,summary,content,cover} = req.body;
+        const {id,title,summary,content} = req.body;
         const postDoc=await Post.findById(id);
         const isAuthor= JSON.stringify(postDoc.author) === JSON.stringify(info.id);
         // res.json({isAuthor,postDoc,info});
@@ -184,7 +127,7 @@ app.put('/post',async (req, res) => {
             title,
             summary,
             content,
-            cover,
+            cover: newPath ? newPath : postDoc.cover,
         });
     res.json(postDoc);
     });
